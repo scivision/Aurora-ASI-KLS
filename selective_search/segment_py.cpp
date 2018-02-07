4c0fce3882b037b3599c9bc289bc2ba35b77ac3a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-07 11:25:38 -0500

    first compiles with Boost 1.63 Numpy

--- selective_search/segment_py.cpp
+++ selective_search/segment_py.cpp
@@ -9,7 +9,7 @@ static int operator<(const rgb& x, const rgb& y)
     return (x.r << 16 | x.g << 8 | x.b) < (y.r << 16 | y.g << 8 | y.b);
 }
 
-static void check_image_format(const boost::numpy::ndarray& input_image)
+static void check_image_format(const boost::python::numpy::ndarray& input_image)
 {
     const int nd = input_image.get_nd();
     if(nd != 3)
@@ -20,14 +20,14 @@ static void check_image_format(const boost::numpy::ndarray& input_image)
     if(depth != 3)
         throw std::runtime_error("input_image must have rgb channel");
 
-    if(input_image.get_dtype() != boost::numpy::dtype::get_builtin<unsigned char>())
+    if(input_image.get_dtype() != boost::python::numpy::dtype::get_builtin<unsigned char>())
         throw std::runtime_error("dtype of input_image must be uint8");
 
-    if(!input_image.get_flags() & boost::numpy::ndarray::C_CONTIGUOUS)
+    if(!input_image.get_flags() & boost::python::numpy::ndarray::C_CONTIGUOUS)
         throw std::runtime_error("input_image must be C-style contiguous");
 }
 
-boost::python::tuple segment(const boost::numpy::ndarray& input_image, float sigma, float c, int min_size)
+boost::python::tuple segment(const boost::python::numpy::ndarray& input_image, float sigma, float c, int min_size)
 {
     check_image_format(input_image);
 
@@ -43,14 +43,14 @@ boost::python::tuple segment(const boost::numpy::ndarray& input_image, float sig
     image<rgb> *seg_result_img = segment_image(&seg_input_img, sigma, c, min_size, &num_css);
 
     // Convert from internal format
-    boost::numpy::ndarray result_image = boost::numpy::empty(input_image.get_nd(), input_image.get_shape(), input_image.get_dtype());
+    boost::python::numpy::ndarray result_image = boost::python::numpy::empty(input_image.get_nd(), input_image.get_shape(), input_image.get_dtype());
     std::copy(seg_result_img->data, seg_result_img->data + w * h, reinterpret_cast<rgb*>(result_image.get_data()));
 
     delete seg_result_img;
-    return boost::python::make_tuple<boost::numpy::ndarray, int>(result_image, num_css);
+    return boost::python::make_tuple<boost::python::numpy::ndarray, int>(result_image, num_css);
 }
 
-boost::python::tuple segment_label(const boost::numpy::ndarray& input_image, float sigma, float c, int min_size)
+boost::python::tuple segment_label(const boost::python::numpy::ndarray& input_image, float sigma, float c, int min_size)
 {
     check_image_format(input_image);
 
@@ -67,7 +67,7 @@ boost::python::tuple segment_label(const boost::numpy::ndarray& input_image, flo
     image<rgb> *seg_result_img = segment_image(&seg_input_img, sigma, c, min_size, &num_css);
 
     // Convert per-region-color to label
-    boost::numpy::ndarray result_label = boost::numpy::empty(2, input_image.get_shape(), boost::numpy::dtype::get_builtin<int>());
+    boost::python::numpy::ndarray result_label = boost::python::numpy::empty(2, input_image.get_shape(), boost::python::numpy::dtype::get_builtin<int>());
     rgb* in_p  = seg_result_img->data;
     int* out_p = reinterpret_cast<int*>(result_label.get_data());
 
@@ -83,12 +83,12 @@ boost::python::tuple segment_label(const boost::numpy::ndarray& input_image, flo
     }
 
     delete seg_result_img;
-    return boost::python::make_tuple<boost::numpy::ndarray, int>(result_label, num_css);
+    return boost::python::make_tuple<boost::python::numpy::ndarray, int>(result_label, num_css);
 }
 
 BOOST_PYTHON_MODULE(segment)
 {
-    boost::numpy::initialize();
+    boost::python::numpy::initialize();
     boost::python::def("segment", segment);
     boost::python::def("segment_label", segment_label);
 }
